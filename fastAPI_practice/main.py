52ea8da1c8e3e52d1697d0ebc8853308a4a8a3ac
NurdauletNU <133967865+NurdauletNU@users.noreply.github.com>
2024-04-14 00:40:01 +0500

    update
    update

--- fastAPI_practice/main.py
+++ fastAPI_practice/main.py
@@ -16,6 +16,7 @@ async def shutdown():
 
 @app.get('/api/users/{user_id}')
 async def delete_user(user_id: int):
-    async with queries.connect_to_database() as connection:
-        await connection.execute('CALL delete_user_by_id($1)', user_id)
+    async with app.state.connection.transaction():
+        await app.state.connection.execute('CALL delete_user_by_id($1)', user_id)
     return {'message': f'User with ID {user_id} deleted successfully'}
+
