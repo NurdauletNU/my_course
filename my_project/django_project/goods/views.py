9ac7ca2abc19f46fce26981e51a7da775c1225b3
NurdauletNU <133967865+NurdauletNU@users.noreply.github.com>
2024-05-09 22:46:25 +0500

    Second pagination option - GET params

--- my_project/django_project/goods/views.py
+++ my_project/django_project/goods/views.py
@@ -4,14 +4,17 @@ from django.http import HttpResponse
 from goods.models import Products
 
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+    
+    page = request.GET.get('page', 1)
+    
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
     
     paginator =Paginator(goods,per_page=3)
-    currenr_page = paginator.page(page)
+    currenr_page = paginator.page(int(page))
         
     context = {
         "title": "Home - Каталог",
