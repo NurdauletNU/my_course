860f077b0c41f58ac2fc061031cc3989ddf0db4b
NurdauletNU <133967865+NurdauletNU@users.noreply.github.com>
2024-05-11 02:47:40 +0500

    Login form and controler are ready

--- my_project/django_project/django_settings/settings.py
+++ my_project/django_project/django_settings/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL, STATIC_ROOT
+from django.conf.global_settings import AUTH_USER_MODEL, MEDIA_ROOT, MEDIA_URL, STATIC_ROOT
 
 
 
@@ -149,3 +149,4 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
