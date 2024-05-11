d73ae1afee1385000434b6e09993de5ca0a71f3a
NurdauletNU <133967865+NurdauletNU@users.noreply.github.com>
2024-05-12 00:24:59 +0500

    Profile form, controller & notifications ready

--- my_project/django_project/users/forms.py
+++ my_project/django_project/users/forms.py
@@ -1,7 +1,7 @@
-from dataclasses import field, fields
+from pyexpat import model
 from re import U
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from users.models import User
 
 class UserLoginForm(AuthenticationForm):
@@ -45,3 +45,19 @@ class UserRegistrationForm(UserCreationForm):
     email = forms.CharField()
     password1 = forms.CharField()
     password2 = forms.CharField()
+    
+    
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = (
+            "image",
+            "first_name",
+            "last_name",
+            "username",
+            "email",)
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
--- my_project/django_project/users/views.py
+++ my_project/django_project/users/views.py
@@ -1,8 +1,10 @@
-from django.contrib import auth
+from email import message
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -14,6 +16,7 @@ def login(request):
             user = auth.authenticate(username=username,password=password)
             if user:
                 auth.login(request,user)
+                messages.success(request, f"{username}, Вы вошли в аккаунт")
                 return HttpResponseRedirect(reverse('django_app:index'))
     else:
         form = UserLoginForm()
@@ -32,6 +35,7 @@ def registration(request):
             form.save()
             user=form.instance
             auth.login(request, user)
+            messages.success(request, f"{user.username}, Вы успешно зарегистрирированы и вошли в аккаунт")
             return HttpResponseRedirect(reverse('django_app:index'))
     else:
         form = UserRegistrationForm()
@@ -47,15 +51,25 @@ def registration(request):
 
 
 
-
+@login_required
 def profile(request):
+    if request.method == "POST":
+        form = ProfileForm(data=request.POST, instance = request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Профайл успешно обновлен")
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance = request.user)
     context ={
-        'title': 'Home - Кабинет'
+        'title': 'Home - Кабинет',
+        'form': form
         }
     return render(request, 'users/profile.html', context=context)
 
-
+@login_required
 def logout(request):
+    messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
     auth.logout(request)
     return redirect(reverse('django_app:index'))
     
