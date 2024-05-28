60bde814a851eb92c2b7c91e2c152a2dfa28f1e4
NurdauletNU <133967865+NurdauletNU@users.noreply.github.com>
2024-05-28 21:55:38 +0500

    login required decorator - redirect to target page

--- my_project/app1/users/views.py
+++ my_project/app1/users/views.py
@@ -16,7 +16,11 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
-                messages.success(request, f"{username}, Вы вошли в аккаунт") 
+                messages.success(request, f"{username}, Вы вошли в аккаунт")
+                
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+                
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form= UserLoginForm()
